917eb48c9152a5f2a04b527c6689aef9563940a2
Asraful Kabir <asrafulkabir017@gmail.com>
2026-04-12 20:55:55 +0600

    push all chang

--- components/app/books/add-book/AddBook.jsx
+++ components/app/books/add-book/AddBook.jsx
@@ -1,5 +1,18 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 export default function AddBook() {
-  return <div></div>;
+  return (
+    <div className="p-3">
+      <div className="border">
+        <div>
+          <p className="border-b">Add book detail</p>
+        </div>
+        {/* book detail adding section */}
+        <div>
+          <form action=""></form>
+        </div>
+      </div>
+    </div>
+  );
 }
